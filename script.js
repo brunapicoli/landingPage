2b62722df16e3d4f43dd3747610daab5d8ce31b0
Bruna <brunatpicoli@outlook.com>
2021-11-19 14:22:14 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showForm() {
+  document.getElementById("name").scrollIntoView();
+  document.getElementById("name").focus();
+}
+
+function validateForm() {
+  var name = document.getElementById("name").value;
+  if (name != '') {
+    alert(`Pronto ${name}! Confira seu e-mail para acessar o material.`)
+  }
+};
